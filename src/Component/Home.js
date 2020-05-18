93ca4f3b5b47fc2765c736047bc4d3d80d856bf6
Manoj-Manikantan <manojmkt127@gmail.com>
2020-05-18 17:39:38 -0600

    First commit

---
+++ src/Component/Home.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Container, Row, Col, Navbar, Nav, Form, Button, FormControl } from "reactstrap"
+import NavbarPage from "./Layout/Navbar"
+
+const Home = () => {
+    return (
+        <Container fluid>
+            <NavbarPage />
+        </Container>
+    )
+}
+
+export default Home;
---
+++ src/Route/route.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../Component/Home";
+import About from "../Component/About";
+import Contact from "../Component/Contact";
+import People from "../Component/People";
+import SampleProjects from "../Component/sampleProjects";
+import Services from "../Component/Services";
+
+class Routes extends Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/people" component={People} />
+                    <Route exact path="/services" component={Services} />
+                    <Route exact path="/sampleProjects" component={SampleProjects} />
+                    <Route exact path="/contact" component={Contact} />
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+export default Routes;
